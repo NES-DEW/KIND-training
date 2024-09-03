84eef434d758f3c243a3ba3ec7e474071aaf5716
Brendan Clarke <brendan.clarke2@nhs.scot>
2024-09-03 15:50:48 +0100

    added r from scratch 1

--- R/feed_block.R
+++ R/feed_block.R
@@ -2,8 +2,24 @@ feed_block <- function(title){
   source(here::here("R/bullet_list.R"))
 
   dat <- readxl::read_excel(here::here("data/KIND_training_feedback.xlsx")) |>
-
-  dplyr::filter(stringr::str_detect(session_title, title))
+    dplyr::filter(stringr::str_detect(session_title, title))
+  
+  vars <- c("Too easy", "About right", "Too hard")
+  
+  results <- dat |>
+    dplyr::mutate(describe = stringr::str_trim(stringr::str_remove_all(describe, "/.*$"))) |>
+    dplyr::count(describe) 
+  
+  plot <- tibble::tibble(
+    describe = vars
+  ) |> 
+    dplyr::left_join(results) |>
+    dplyr::mutate(n = tidyr::replace_na(n, 0), 
+           describe = factor(describe, levels = vars)) |>
+    ggplot2::ggplot() +
+    ggplot2::geom_col(ggplot2::aes(x = factor(describe), y = n), fill = "steelblue") +
+    ggplot2::geom_label(ggplot2::aes(label = describe, x = describe, y = max(n)/2), size = 3) +
+    ggplot2::theme_void()
   
   if (nrow(dat) == 0) {
     return(cat("No feedback found for this session"))
@@ -15,11 +31,14 @@ feed_block <- function(title){
     cat("  \n## Previous attendees have said...  \n")
     
     vec <- c(paste0(nrow(dat), " previous attendees have left feedback"),
-             paste0(rite, " said that this session was pitched correctly"), 
-             paste0(rec, " would recommend this session to a colleague"))
+             paste0(rec, " would recommend this session to a colleague"),
+             paste0(rite, " said that this session was pitched correctly:  \n"))
     
     bullet_list(vec)
     
+    print(plot)
+    
+    cat("  \n")
     cat("  \n")
     cat(":::{.callout-note}")
     cat("  \n")
@@ -36,3 +55,8 @@ feed_block <- function(title){
   
     }
 }
+
+
+
+
+
