484ef852566847bfdd10f769f420f53da8c8b629
Brendan Clarke <brendan.clarke2@nhs.scot>
2025-08-27 13:51:56 +0100

    updating next-session blocks

--- R/next_sesh.R
+++ R/next_sesh.R
@@ -7,16 +7,28 @@ next_sesh <- function(page_title){
   
   if(nrow(forthcoming) > 0){
     
+    cat("::: {.callout-tip collapse=false appearance='default' icon=true}  \n")
+    
+    cat("## Forthcoming session(s)  \n")
+    
     KINDR::training_sessions(output_type = "tibble") |>
-      dplyr::filter(title == forthcoming$`session title`,
+      dplyr::filter(title %in% forthcoming$`session title`,
                     start >= min(forthcoming$start)) |>
       dplyr::mutate(title = paste0("<a href='", url, "'>", title, "</a>")) |>
       dplyr::select(`Booking link` = title, Date = friendly_date) |>
-      kableExtra::kbl(escape = FALSE,
-                      caption = "Forthcoming sessions(s)") |>
-      print()
+      kableExtra::kbl(escape = FALSE) |>
+      cat()
+    
+    cat("  \n")
+    
+    cat(":::  \n")
 
   }
 
 }
 # next_sesh("Pivot tables and pivot charts (intermediate Excel session 4)")
+
+# :::{.callout-tip collapse=false appearance='default' icon=true}
+
+## Forthcoming sessions(s)
+# :::
