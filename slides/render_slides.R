37036919d0a93111b56d77b4368c312a73b2e1ee
Brendan Clarke <brendan.clarke2@nhs.scot>
2025-09-15 13:29:44 +0100

    tweaked render function

--- slides/render_slides.R
+++ slides/render_slides.R
@@ -1,26 +1,26 @@
 # slide render script
 # created an appropriately dated version of the slides in this slide subdirectory
 
-# set the file and the session date ----
+render_slides <- function(file_path, session_date){
+  # fn <- "excel/references_and_names_in_excel.qmd" # path relative to project wd
+  session_date <- "2025-09-17" #in this format, don't fool with it
+  
+  out_fn <- paste0("slides_",
+                   tools::file_path_sans_ext(basename(file_path)),
+                   ".html")
+  
+  site_fn <- paste0("_site/", 
+                    stringr::str_extract(file_path, ".*\\/"),
+                    out_fn)
+  
+  quarto::quarto_render(file_path,
+                        output_format = "revealjs",
+                        output_file = out_fn,
+                        execute_params = list(sl_date = session_date),
+                        metadata_file = "slides/slide_render.yml")
+  
+  file.copy(site_fn, paste0("slides/", out_fn), overwrite = T)
+  unlink(site_fn)
+}
 
-fn <- "excel/references_and_names_in_excel.qmd" # path relative to project wd
-session_date <- "2025-09-17" #in this format, don't fool with it
-
-out_fn <- paste0("slides_",
-       tools::file_path_sans_ext(basename(fn)),
-       ".html")
-
-site_fn <- paste0("_site/", 
-       stringr::str_extract(fn, ".*\\/"),
-       out_fn)
-
-quarto::quarto_render(fn,
-                      output_format = "revealjs",
-                      output_file = out_fn,
-                      execute_params = list(sl_date = session_date),
-                      metadata_file = "slides/slide_render.yml")
-
-file.copy(site_fn, paste0("slides/", out_fn), overwrite = T)
-unlink(site_fn)
-
-# quarto::quarto_render(fn, output_format = "html")
+render_slides("excel/references_and_names_in_excel.qmd", "2025-09-17")
