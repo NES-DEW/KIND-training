1b05ea5f5359353b44d76b82ea9c118aa8691649
Brendan Clarke <brendan.clarke2@nhs.scot>
2026-01-16 12:47:06 +0000

    re-render after schedule change

--- slides/render_slides.R
+++ slides/render_slides.R
@@ -2,14 +2,13 @@
 # created an appropriately dated version of the slides in this slide subdirectory
 
 render_slides <- function(file_path, session_date){
-  out_fn <- paste0(tools::file_path_sans_ext(basename(file_path)),
+  out_fn <- paste0("slides_",
+                   tools::file_path_sans_ext(basename(file_path)),
                    "_",
                    as.character(session_date), 
                    ".html")
   
-  site_fn <- paste0("docs/", 
-                    stringr::str_extract(file_path, ".*\\/"),
-                    out_fn)
+  site_fn <- paste0("docs/", stringr::str_extract(file_path, ".*\\/"), out_fn)
   
   quarto::quarto_render(file_path,
                         output_format = "revealjs",
@@ -21,4 +20,7 @@ render_slides <- function(file_path, session_date){
   unlink(site_fn)
 }
 
-render_slides("skills/writing_effective_surveys.qmd", "2025-11-20")
+render_slides("excel/excel_first_steps.qmd", "2026-01-13")
+# render_slides("excel/tidy_data_in_excel.qmd", "2026-01-20")
+# render_slides("excel/references_and_names_in_excel.qmd", "2026-01-27")
+
