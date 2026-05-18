4dc71952deda93c36edc86330ce62d10b6fd18ba
Brendan Clarke <brendan.clarke2@nhs.scot>
2026-05-18 12:18:59 +0100

    now using freeze

--- R/next_sesh.R
+++ R/next_sesh.R
@@ -1,31 +1,32 @@
 
 next_sesh <- function(page_title){
   
-  forthcoming <- readr::read_csv(here::here("data/training_schedule.csv")) |>
-    dplyr::filter(`session title` == page_title, start >= lubridate::now()) 
-  
-  
-  if(nrow(forthcoming) > 0){
-    
-    cat("  \n")
-    
-    cat("::: {.callout-tip collapse=false appearance='default' icon=true}  \n")
-    
-    cat("## Forthcoming session(s)  \n")
-    
-    KINDR::training_sessions(output_type = "tibble", start_date = as.character(lubridate::today()-1)) |>
-      dplyr::filter(title %in% forthcoming$`session title`,
-                    start >= min(forthcoming$start)) |>
-      dplyr::mutate(title = paste0("<a href='", url, "'>", title, "</a>")) |>
-      dplyr::select(`Booking link` = title, Date = friendly_date) |>
-      kableExtra::kbl(escape = FALSE) |>
-      cat()
-    
-    cat("  \n")
-    
-    cat(":::  \n")
-
-  }
+  # forthcoming <- readr::read_csv(here::here("data/training_schedule.csv")) |>
+  #   dplyr::filter(`session title` == page_title, start >= lubridate::now()) 
+  # 
+  # 
+  # if(nrow(forthcoming) > 0){
+  #   
+  #   cat("  \n")
+  #   
+  #   cat("::: {.callout-tip collapse=false appearance='default' icon=true}  \n")
+  #   
+  #   cat("## Forthcoming session(s)  \n")
+  #   
+  #   KINDR::training_sessions(output_type = "tibble", start_date = as.character(lubridate::today()-1)) |>
+  #     dplyr::filter(title %in% forthcoming$`session title`,
+  #                   start >= min(forthcoming$start)) |>
+  #     dplyr::mutate(title = paste0("<a href='", url, "'>", title, "</a>")) |>
+  #     dplyr::select(`Booking link` = title, Date = friendly_date) |>
+  #     kableExtra::kbl(escape = FALSE) |>
+  #     cat()
+  #   
+  #   cat("  \n")
+  #   
+  #   cat(":::  \n")
+  # 
+  # }
+  cat("  \n")
 
 }
 # next_sesh("Pivot tables and pivot charts (intermediate Excel session 4)")
